7ebd7c1c6e00163a1539388c482dc21ed9f873c4
baokhue <117093775+baokhue@users.noreply.github.com>
2023-02-10 21:15:32 +0700

    Update Manager.java

--- Interface/src/Resizeable/Manager.java
+++ Interface/src/Resizeable/Manager.java
@@ -1,12 +1,19 @@
 package Resizeable;
 
 public class Manager {
-    public static void main(String[] args) {
-        Circle circle = new Circle(5);
-        Rectangle rectangle = new Rectangle(2, 5);
-        Square square = new Square(4,6);
-        circle.resize(5);
-        rectangle.resize(10);
-        square.resize(15);
+     public static void main(String[] args) {
+        Shape[] shapes = new Shape[3];
+
+        shapes[0] = new Circle(3);
+        shapes[1] = new Square(2);
+        shapes[2] = new Rectangle(3,2);
+
+
+
+        for (Shape shape : shapes) {
+            double percent = Math.random() * 100;
+            shape.resize(percent);
+            System.out.println(shape);
+        }
     }
 }
