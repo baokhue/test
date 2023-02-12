2c59e5cfc1a502561e253b81b826a0a45add519f
baokhue <khue49945@donga.edu.vn>
2023-02-12 17:33:56 +0700

    update

---
+++ CountCharacter/src/count_character/Index.java
@@ -0,0 +1,36 @@
+package count_character;
+
+public class Index {
+    private int count = 0;
+    private String str = "CodeGym Da Nang";
+
+    public Index() {
+    }
+
+    public Index(int count, String str) {
+        this.count = count;
+        this.str = str;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+
+    public void getCountCharacter() {
+        for (int i = 0; i < str.length(); i++) {
+
+        }
+    }
+}
---
+++ CountCharacter/src/count_character/Manager.java
@@ -0,0 +1,8 @@
+package count_character;
+
+public class Manager {
+    public static void main(String[] args) {
+        Index index = new Index();
+
+    }
+}
