1da5ec83ab2ebe4a8de9503af75c1321bb2d25ab
baokhue <khue49945@donga.edu.vn>
2023-02-24 18:49:20 +0700

    Optional exe

---
+++ DSAList/src/stack/BinarySystem.java
@@ -0,0 +1,25 @@
+package stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class BinarySystem {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please enter the number you want to convert to binary.");
+        int numberDecimal = sc.nextInt();
+
+        int sumBinary;
+        while (numberDecimal > 0) {
+            sumBinary = numberDecimal % 2;
+            stack.push(sumBinary);
+            numberDecimal /= 2;
+        }
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop());
+        }
+
+    }
+}
